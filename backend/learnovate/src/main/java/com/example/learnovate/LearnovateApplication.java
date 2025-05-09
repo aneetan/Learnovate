04b452f6ec1429f41cad881512eac5beaa8092e2
aneetan <anitaneupane2061@gmail.com>
2025-05-09 20:18:47 +0545

    Project created

---
+++ backend/learnovate/src/main/java/com/example/learnovate/LearnovateApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnovateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnovateApplication.class, args);
+	}
+
+}
---
+++ backend/learnovate/src/test/java/com/example/learnovate/LearnovateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnovateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
